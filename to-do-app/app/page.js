4a03a98deaed74b189ec395989159c8c1b7056bb
Reetesh07 <reeteshmore24@gmail.com>
2025-09-22 00:31:08 +0530

    Update page.js

--- to-do-app/app/page.js
+++ to-do-app/app/page.js
@@ -50,6 +50,7 @@ const page = () => {
                     className="border-4 border-zinc-800 px-3 py-2 rounded-lg w-1/3 mx-2"
                     value={title}
                     onChange={(e) => settitle(e.target.value)}
+                    required
                 />
 
                 <input
